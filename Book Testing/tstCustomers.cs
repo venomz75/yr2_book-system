87c9b0352d7fbdfeb274719c2a16248cdce39765
David Dorey-Rodriguez <P2413606@my365.dmu.ac.uk>
2020-02-10 16:22:55 +0000

    Added testing and class for customer table.

---
+++ Book Testing/tstCustomers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using BookClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Book_Testing
+{
+    [TestClass]
+    public class tstCustomers
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomers ACustomer = new clsCustomers();
+            //test to see that it exists
+            Assert.IsNotNull(ACustomer);
+        }
+
+
+
+
+    }
+}
