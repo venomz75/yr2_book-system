bea023b012b58cc1054fbf25ae75e482fe74726b
Christopher Freeman <P17169519@my365.dmu.ac.uk>
2020-02-10 17:00:28 +0000

    Added test class for review table

---
+++ Book Testing/UnitTest2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Book_Testing
+{
+    [TestClass]
+    public class clsReview
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsReview AnReview = new clsReview();
+
+            Assert.IsNotNull(AnReview);
+        }
+    }
+}
