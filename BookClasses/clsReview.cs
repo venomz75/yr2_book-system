be0e06b2b2957aa87b2f5310b34695f6a19a02e7
Christopher Freeman <P17169519@my365.dmu.ac.uk>
2020-02-17 18:23:06 +0000

    Wk 19 Testing and start up web app form

--- BookClasses/clsReview.cs
+++ BookClasses/clsReview.cs
@@ -1,9 +1,14 @@
-﻿namespace BookClasses
+﻿using System;
+
+namespace BookClasses
 {
     public class clsReview
     {
-        public clsReview()
-        {
-        }
+        public bool Active { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int ReviewNo { get; set; }
+        public string ReviewerName { get; set; }
+        public int Star { get; set; }
+        public bool WouldUse { get; set; }
     }
 }
